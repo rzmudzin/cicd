a16f4c615d1fcc28298539e30578047c530e34dc
rob <rob@rob.com>
2022-11-11 10:40:06 -0700

    Switching to zsh

--- build-app.sh
+++ build-app.sh
@@ -1,5 +1,8 @@
+#!/bin/zsh
+
 log=$2
 echo "Starting Build Process using build file $1" >> $2
+echo "${BASH_VERSION}" >> $2
 
 data=$(python3 json-parse.py "$1")
 echo $data >> $2
@@ -10,7 +13,7 @@ parts=($(echo $data | tr "" "\n"))
 len=${#parts[@]}
 echo "Len: $len" >> $2
 
-echo "Indvidual Values..." >> $2
+echo "Extracted Build Values..." >> $2
 name=""
 value=""
 COUNTER=0
@@ -24,10 +27,11 @@ while [  $COUNTER -lt $len ]; do
     fi    
     let COUNTER=COUNTER+1 
 done
+echo "$COUNTER Build Values Extracted" >> $2
 
-echo  "Key Value Pairs..." >> $2
+echo  "Build Parameters..." >> $2
 for key in "${!buildArgs[@]}"; do
     echo "$key ${buildArgs[$key]}" >> $2
-done
+done 
 
 echo "Build Completed" >> $2
--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
+#!/bin/zsh
+
 buildId=$1
 echo "Build Request Id: $1" > "$buildId.log"
 cd cicd
-bassh ./build-app.sh "$buildId.json" "$buildId.log"
+./build-app.sh "$buildId.json" "$buildId.log"
 echo "put $buildId.json $buildId.json" > "sftp.$buildId.upload"
 sftp -b "sftp.$buildId.upload" rzmudzin@192.168.64.15
 ssh rzmudzin@192.168.64.15 ./build.sh "$buildId"
